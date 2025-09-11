69d95baee3df7d3522d7e02416197f2a6c1f6b3d
Ideraoluwa-No <nimotalahiolakanmi@gmail.com>
2025-09-11 23:20:20 +0100

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -61,7 +61,7 @@ const Header = () => {
 
         {/* Auth Buttons */}
 
-        <div className="flex space-x-4">
+        <div className="hidden md:flex space-x-4">
           <Link
             to="/login"
             className="px-4 py-2 text-blue-600 hover:bg-blue-50 rounded-lg"
